c9aa7cb196aa1de14d6887c1a1c4c76103fc179a
JasonPC <jasonwong287962@gmail.com>
2022-12-15 23:27:08 -0600

    updates board on js and DOM when clicked on the tic tac toe game

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,77 +1,135 @@
+// I have a gameBoard Module
+// gameBoard.createBoard() --> creates an array from 0 to 8 representing the 9 boxes in tic tac toe
+//   each box is represented within gameBoard module by a boardRow Object 
+// so gameBoard.getBoard()[0] returns a boardBox. 
+// Each boardBox is a corner in tictactoe that can be setClickTrue();
+// so gameBoard.getBoard()[0].setClickTrue();
+// get the Board current results with gameBoard.getBoard();
+
+const displayController = (() =>{
+    //need to alternate between characters
+    var turn = 1;
 
 const gameBoard = (() => {
     //a gameboard has 3 boxes that can be clicked
    
 
-    function boardRow(num){
+    function boardBox(num,playerNum){
         let clicked = false;
-        const boxRow =[num,num+1,num+2];
+        const boxRow =[num];
+        var playerNum = 'O';
+        function setClickTrue(player){
+            clicked = true; 
+            playerNum = player;
 
-        function click(){
-            clicked = true;
         }
+        function getClicked(){
+            return clicked;
+
+        }
+        function getPlayerNum(){
+            return playerNum;
+        }
+
         
-        return {boxRow,click};        
+        return {boxRow,setClickTrue,getClicked,getPlayerNum};        
     }
     // board contains the 9 box
     const board = [];
     
 function createBoard(){
     
-
-        for(let i = 1; i<=3; i++){
+    for(let o =1; o<=3;o++)
+        for(let i = 0; i<3; i++){
                 
              
-                if(i === 1){  
+                if(o === 1){  
                     total = 0;  
                 }
-            else if(i===2){
-                    total =2;
+            else if(o===2){
+                    total =3;
                 }else{
-                    total = 4;  
+                    total = 6;  
                 }
                 total+=i;
-                board.push(boardRow(total));
+                board.push(boardBox(total));
             }
 }
-function updateBoard(num){
-    console.log(gameBoard.getBoard()[2].boxRow[2]);
-    for(box in gameBoard.getBoard().boxRow){
-    
-    }
+function updateBoard(num,player){
+    gameBoard.getBoard()[num].setClickTrue(player);
 }   
 function getBoard(){
     return board;
 
 }
 // starting game
-    const startGame = createBoard();
+    
     // console.log(startGame.getBoardRow());
 
 
-    return {updateBoard, getBoard};
+    return {updateBoard, getBoard, createBoard};
 })();
+
+
 
 
 
 
 
 const player = (number) =>{
+    if(number === 1){
+        var shape = 'O';
+    }
+    else {
+        shape = 'X';
+    }
+    const playerNumber = shape;
+    console.log(playerNumber);
+    function getPlayerNumber(){
+        return playerNumber;
+    }
     
+    return {getPlayerNumber};
 }
 
-const displayController = (() =>{
-    let winner;
-    
+
+//players and board initiated 
+const player1 = player(1);
+const player2 = player(2);
+gameBoard.createBoard();
+
+ 
+let ticButtons = document.querySelectorAll('button');
+
+for(const ticButton of ticButtons)
+ticButton.addEventListener('click',e =>{
+
+    var num=  ticButton.id
+    if((turn % 2) === 0){
+     
+        gameBoard.updateBoard(num,player2.getPlayerNumber());
+        ticButton.textContent = player2.getPlayerNumber();
+        console.log( player1.getPlayerNumber());
+     }
+    else{
+     
+        gameBoard.updateBoard(num,player1.getPlayerNumber());
+        ticButton.textContent = player1.getPlayerNumber();
+       
+    }
+    turn++;
+    ticButton.classList.add('clicked');
+     
+});
+
+
+
+
+
+
+
+
+
+
 
 })();
-
-
-gameBoard.updateBoard(1);
-
-
-
-
-
-
-
